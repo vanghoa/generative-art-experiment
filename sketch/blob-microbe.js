9b132afc8da45cb6e3a0070f01c4814d14327527
vanghoa <baoanh1buinguyen@gmail.com>
2022-08-15 02:16:17 +0800

    letsfuckingo

--- sketch/blob-microbe.js
+++ sketch/blob-microbe.js
@@ -2,10 +2,11 @@ const inc = 0.07;
 let zoff = 0
 let arr = [];
 let trace;
-const size_ = 1000;
+const size_ = innerHeight;
 const offsetsize = 100;
-const gap = 1000/30;
-const gap_ = gap*2;
+const dist_gap = size_/30;
+const pos_rand_gap = dist_gap*2;
+const radius_ = 100;
 
 function setup() {
   pixelDensity(1);
@@ -16,12 +17,12 @@ function setup() {
 
   let yoff = 0;
   
-  for (let y = -height/2 + offsetsize; y < height/2 - offsetsize; y+=gap) {
-    for (let x = -width/2 + offsetsize; x < width/2 - offsetsize; x+=gap) {
-      arr.push({x: random(x-gap_,x+gap_), 
-                y: random(y-gap_,y+gap_),
-                x_: random(x-gap_,x+gap_), 
-                y_: random(y-gap_,y+gap_)
+  for (let y = -height/2 + offsetsize; y < height/2 - offsetsize; y+=dist_gap) {
+    for (let x = -width/2 + offsetsize; x < width/2 - offsetsize; x+=dist_gap) {
+      arr.push({x: random(x-pos_rand_gap,x+pos_rand_gap), 
+                y: random(y-pos_rand_gap,y+pos_rand_gap),
+                x_: random(x-pos_rand_gap,x+pos_rand_gap), 
+                y_: random(y-pos_rand_gap,y+pos_rand_gap)
                }); 
     }
   }
@@ -37,36 +38,32 @@ function draw() {
   noStroke();
   noiseDetail(3, 0.6);
   
-  for (let y = -height/2 + offsetsize; y < height/2 - offsetsize; y+=gap) {
+  for (let y = -height/2 + offsetsize; y < height/2 - offsetsize; y+=dist_gap) {
     let xoff = 0;
     
-    for (let x = -width/2 + offsetsize; x < width/2 - offsetsize; x+=gap) {
+    for (let x = -width/2 + offsetsize; x < width/2 - offsetsize; x+=dist_gap) {
       let r = noise(xoff, yoff, zoff);
       
       if (r < 0.6) {
-        if (r > 0.55) {
+        if (r > 0.45) {
           push();
           translate(arr[count].x_, arr[count].y_, 0);
           normalMaterial();
-          //ambientMaterial(255, 0, 255);
-          //ambientLight(0);
           sphere(map(r,0.3,0.5,1,2));
           pop();
         }
         
-        arr[count].x = random(x-gap_,x+gap_);
-        arr[count].y = random(y-gap_,y+gap_);
+        arr[count].x = random(x-pos_rand_gap,x+pos_rand_gap);
+        arr[count].y = random(y-pos_rand_gap,y+pos_rand_gap);
       } else {
         push();
         translate(arr[count].x, arr[count].y, map(r,0.2,1,-100,200));
         normalMaterial();
-        //ambientMaterial(255, 0, 255);
-        //ambientLight(0);
-        sphere(pow(r, (1-r)*10)*70);
+        sphere(pow(r, (1-r)*10)*radius_);
         pop();
         
-        arr[count].x_ = random(x-gap_,x+gap_);
-        arr[count].y_ = random(y-gap_,y+gap_);
+        arr[count].x_ = random(x-pos_rand_gap,x+pos_rand_gap);
+        arr[count].y_ = random(y-pos_rand_gap,y+pos_rand_gap);
       }
       count++;
       
@@ -75,11 +72,12 @@ function draw() {
     yoff += inc;
   }
 
+  
   loadPixels();
   trace.loadPixels();
 
   for (let x = 1; x < width; x++ ) {
-    for (let y = 0; y < height; y++ ) {
+    for (let y = 1; y < height; y++ ) {
       let loc = (x + y * width) * 4;
       // The functions red(), green(), and blue() pull out the three color components from a pixel.
       let r = pixels[loc   ]; 
@@ -87,23 +85,37 @@ function draw() {
       let b = pixels[loc + 2];
       
       // Pixel to the left location and color
-      let leftLoc = ((x - 1) + y * width) * 4;
-      let rleft = pixels[leftLoc   ]; 
-      let gleft = pixels[leftLoc + 1];
-      let bleft = pixels[leftLoc + 2];      
+      let left = pos_compare(-1,0);
+      let top = pos_compare(0,-1);
+      let right = pos_compare(1,0);
+      let bot = pos_compare(0,1);
+
+      //let max_diff = max(left,top,right,bot);
+      let ave_diff = (left + top + right + bot)/4;
+
       // New color is difference between pixel and left neighbor
-      let diff = abs((r+g+b)/3 - (rleft+gleft+bleft)/3);
-      diff = (diff > 8) ? 255 : 0;
+      let diff = (ave_diff > 6) ? ave_diff*40 : ave_diff;
       trace.pixels[loc    ] = diff;
       trace.pixels[loc + 1] = diff;
       trace.pixels[loc + 2] = diff;
       trace.pixels[loc + 3] = 255; // Always have to set alpha
+
+      function pos_compare(x_, y_) {
+        let loc = (x + x_ + (y + y_) * width) * 4;
+        let pix = {
+          r: pixels[loc    ],
+          g: pixels[loc + 1],
+          b: pixels[loc + 2],
+        }
+        return abs((r+g+b)/3 - (pix.r + pix.g + pix.b)/3);
+      }
+
     }
   }
   trace.updatePixels();
-  //translate(-width/2,-height/2);
   clear();
   image(trace,-size_/2,-size_/2);
+  
 
   zoff += 0.02;
   noLoop();
